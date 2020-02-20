70696af8a83206e9b13831b1a7369349f4bf444c
David Bautista <dj.bautista10@uniandes.edu.co>
2020-02-19 19:43:51 -0500

    first commit

---
+++ back-end/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var playersRouter = require('./routes/players');
+var templatesRouter = require('./routes/templates');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/players', playersRouter);
+app.use('/templates', templatesRouter);
+
+module.exports = app;
