483e376d4a8d5e7cacfc5924df7d9ae66b40cae7
David Bautista <dj.bautista10@uniandes.edu.co>
2020-02-25 18:04:15 -0500

    Scrapper optimizado

--- back-end/utils/fantasy_json_scrapper.js
+++ back-end/utils/fantasy_json_scrapper.js
@@ -1,11 +1,70 @@
 "user strict";
+
+const structure = {
+  PlayerID: 14536,
+  Seasons: {
+    Season: 2015,
+    Played: 16,
+    PassingCompletions: 329,
+    PassingAttempts: 483,
+    PassingCompletionPercentage: 68.1,
+    PassingYards: 4024,
+    PassingYardsPerAttempt: 8.3,
+    PassingTouchdowns: 34,
+    PassingInterceptions: 8,
+    PassingRating: 110.12,
+    RushingAttempts: 103,
+    RushingYards: 553,
+    RushingYardsPerAttempt: 5.4,
+    RushingTouchdowns: 1,
+    Receptions: 0,
+    ReceivingTargets: 0,
+    ReceivingYards: 0,
+    ReceptionPercentage: 0,
+    ReceivingTouchdowns: 0,
+    ReceivingLong: 0,
+    ReceivingYardsPerTarget: 0,
+    ReceivingYardsPerReception: 0,
+    Fumbles: 7,
+    FumblesLost: 3,
+    FieldGoalsMade: 0,
+    FieldGoalsAttempted: 0,
+    FieldGoalPercentage: 0,
+    FieldGoalsLongestMade: 0,
+    ExtraPointsMade: 0,
+    ExtraPointsAttempted: 0,
+    TacklesForLoss: 0,
+    Sacks: 0,
+    QuarterbackHits: 0,
+    Interceptions: 0,
+    FumblesRecovered: 0,
+    Safeties: 0,
+    DefensiveTouchdowns: 0,
+    SpecialTeamsTouchdowns: 0,
+    SoloTackles: 0,
+    AssistedTackles: 0,
+    SackYards: 0,
+    PassesDefended: 0,
+    FumblesForced: 0,
+    FantasyPoints: 336.26,
+    FantasyPointsPerGame: 21,
+    TotalTackles: 0
+  },
+  PlayerUrlString: "/nfl/russell-wilson-fantasy/14536",
+  Name: "Russell Wilson",
+  ShortName: "R. Wilson",
+  Position: "QB",
+  TeamUrlString: "/nfl/team-details/SEA",
+  Team: "SEA"
+};
+
 const fetch = require("node-fetch");
 const fs = require("fs");
-var current_players = [];
-var current_total = 0;
+let current_players = {};
+let current_total = 0;
 
-var max_players = process.argv[2] || 2000;
-var limit = max_players < 2000 ? max_players * 18 : 29045;
+let max_players = process.argv[2] || 2000;
+let limit = max_players < 2000 ? max_players * 5 : 8408;
 
 console.log("argv", process.argv[2]);
 console.log("max", max_players);
@@ -20,7 +79,7 @@ const writePlayersJSONArray = () =>
         console.log(err);
       }
       console.log(`finished :) ${current_total} players written ...
-    final lenght: ${current_players.length} ...
+    final lenght: ${Object.keys(current_players).length} ...
     final total: ${current_total}`);
     }
   );
@@ -28,6 +87,7 @@ const writePlayersJSONArray = () =>
 const getPlayers = season => {
   const body = `sort=FantasyPoints-desc&pageSize=${max_players}&group=&filter=&filters.position=&filters.team=&filters.teamkey=&filters.season=${season}&filters.seasontype=1&filters.scope=1&filters.subscope=1&filters.redzonescope=&filters.scoringsystem=&filters.leaguetype=&filters.searchtext=&filters.week=&filters.startweek=&filters.endweek=&filters.minimumsnaps=&filters.teamaspect=&filters.stattype=&filters.exportType=&filters.desktop=&filters.dfsoperator=&filters.dfsslateid=&filters.dfsslategameid=&filters.dfsrosterslot=&filters.page=&filters.showfavs=&filters.posgroup=&filters.oddsstate=&filters.showall=&filters.aggregatescope=1&filters.rangescope=&filters.range=3`;
   console.log("body", body);
+
   fetch("https://fantasydata.com/FantasyStatsNFL/FantasyStats_Read", {
     credentials: "include",
     headers: {
@@ -52,13 +112,42 @@ const getPlayers = season => {
         return;
       }
       console.log("appending data with length", data.Data.length);
-      current_players = current_players.concat(data.Data);
+      data.Data.forEach(player => {
+        // console.log("playerid", +player.PlayerID);
+        let posiblePlayer = current_players[+player.PlayerID];
+
+        if (posiblePlayer) {
+          let season = {};
+          for (let attr in structure["Seasons"]) {
+            season[attr] = player[attr];
+          }
+          posiblePlayer["Seasons"].push(season);
+        } else {
+          let newPlayer = {};
+          for (let attr in structure) {
+            if (attr === "Seasons") {
+              let season = {};
+              for (let seasonAttr in structure[attr]) {
+                season[seasonAttr] = player[seasonAttr];
+              }
+              let seasons = [];
+              seasons.push(season);
+              newPlayer[attr] = seasons;
+            } else {
+              newPlayer[attr] = player[attr];
+            }
+          }
+          posiblePlayer = newPlayer;
+          current_players[+player.PlayerID] = posiblePlayer;
+        }
+      });
+      // current_players = current_players.concat(data.Data);
       current_total += data.Data.length;
       console.log(
         `data downloaded of season ${season} .. 
         obtained ${data.Data.length} players of ${data.Total} ... 
         current total: ${current_total} ...
-        current length: ${current_players.length}`
+        current length: ${Object.keys(current_players).length}`
       );
 
       if (current_total >= limit) {
@@ -67,6 +156,6 @@ const getPlayers = season => {
     });
 };
 
-for (var season = 2019; season >= 2002; season--) {
+for (let season = 2019; season >= 2015; season--) {
   getPlayers(season);
 }
