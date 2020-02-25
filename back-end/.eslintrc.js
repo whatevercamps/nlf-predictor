4e29d60a6bc8c0d25b2bc0648f0c1c0a315d6fec
Juan Felipe Torres <jf.torresp@uniandes.edu.co>
2020-02-25 17:41:17 -0500

    Commit players page

--- back-end/.eslintrc.js
+++ back-end/.eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-    indent: ['error', 4],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always']
