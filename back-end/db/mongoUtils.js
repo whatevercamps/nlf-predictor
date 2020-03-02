0ea6804325f7ad75e8f59a5da294bc13f4274d9d
Juan Felipe Torres <jf.torresp@uniandes.edu.co>
2020-03-01 19:47:16 -0500

    Detail linked, remaining some functions

--- back-end/db/mongoUtils.js
+++ back-end/db/mongoUtils.js
@@ -2,7 +2,7 @@
 
 const MongoClient = require("mongodb").MongoClient;
 const dotenv = require("dotenv");
-// var ObjectId = require("mongodb").ObjectID;
+const ObjectID = require("mongodb").ObjectID;
 dotenv.config();
 
 const mongoUtils = () => {
@@ -26,7 +26,6 @@ const mongoUtils = () => {
 
     return players
       .find(query)
-      .limit(10)
       .toArray()
       .finally(() => {
         console.log("cerrando cliente");
@@ -34,12 +33,23 @@ const mongoUtils = () => {
       });
   };
 
+  mu.findOneByID = (client, id) => {
+    const players = client.db(dbName).collection("players");
+
+    // when searching by id we need to create an ObjectID
+    return players
+      .findOne({ _id: new ObjectID(id) })
+      .finally(() => {
+        console.log("cerrando cliente");
+        client.close()
+      });
+ };
+
   mu.getPlayers2 = (client, query) => {
     const players = client.db(dbName).collection("players");
 
     return players
       .find(query)
-      .limit(10)
       .toArray()
       .finally(() => {
         console.log("cerrando cliente");
